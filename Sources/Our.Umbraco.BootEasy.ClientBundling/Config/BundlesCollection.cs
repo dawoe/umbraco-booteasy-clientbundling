1c982c04d3b5caf3cc65d106bff3a69a1c0c4b06
Dave Woestenborghs <dave.woestenborghs@gmail.com>
2016-03-15 19:43:31 +0100

    Added bundles collection config element

---
+++ Sources/Our.Umbraco.BootEasy.ClientBundling/Config/BundlesCollection.cs
@@ -0,0 +1,73 @@
+﻿namespace Our.Umbraco.BootEasy.ClientBundling.Config
+{
+    using System;
+    using System.Configuration;
+
+    /// <summary>
+    /// The bundles collection config element
+    /// </summary>
+    public class BundlesCollection : ConfigurationElementCollection
+    {
+        /// <summary>
+        /// The tag name.
+        /// </summary>
+        internal const string TagName = "bundle";
+
+        /// <summary>
+        /// Gets the type of the <see cref="T:System.Configuration.ConfigurationElementCollection"/>.
+        /// </summary>
+        /// <returns>The <see cref="T:System.Configuration.ConfigurationElementCollectionType"/> of this collection.</returns>
+        public override ConfigurationElementCollectionType CollectionType
+        {
+            get
+            {
+                return ConfigurationElementCollectionType.BasicMapAlternate;
+            }
+        }
+
+        /// <summary>
+        /// Gets the element name.
+        /// </summary>
+        protected override string ElementName
+        {
+            get
+            {
+                return TagName;
+            }
+        }
+
+        /// <summary>
+        /// Gets a value indicating whether the <see cref="T:System.Configuration.ConfigurationElementCollection"/> object is read only.
+        /// </summary>
+        /// <returns>
+        /// true if the <see cref="T:System.Configuration.ConfigurationElementCollection"/> object is read only; otherwise, false.
+        /// </returns>
+        public override bool IsReadOnly()
+        {
+            return false;
+        }
+
+        /// <summary>
+        /// Indicates whether the specified <see cref="T:System.Configuration.ConfigurationElement"/> exists in the <see cref="T:System.Configuration.ConfigurationElementCollection"/>.
+        /// </summary>
+        /// <param name="elementName">The name of the element to verify.</param>
+        /// <returns>
+        /// true if the element exists in the collection; otherwise, false. The default is false.
+        /// </returns>
+        protected override bool IsElementName(string elementName)
+        {
+            return elementName.Equals(TagName, StringComparison.InvariantCultureIgnoreCase);
+        }
+
+       
+        protected override ConfigurationElement CreateNewElement()
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override object GetElementKey(ConfigurationElement element)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
