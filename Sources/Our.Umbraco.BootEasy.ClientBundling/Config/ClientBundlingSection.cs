b8dff6b9e4df63422f2130be7f25237248ebdcf2
Dave Woestenborghs <dave.woestenborghs@gmail.com>
2016-03-15 19:35:16 +0100

    Added config section class

---
+++ Sources/Our.Umbraco.BootEasy.ClientBundling/Config/ClientBundlingSection.cs
@@ -0,0 +1,11 @@
+﻿namespace Our.Umbraco.BootEasy.ClientBundling.Config
+{
+    using System.Configuration;
+
+    /// <summary>
+    /// The client bundling configuration section.
+    /// </summary>
+    public class ClientBundlingSection : ConfigurationSection
+    {
+    }
+}
