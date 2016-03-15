a4564f6195defde99243c5f49eb934f4ed3348a4
Dave Woestenborghs <dave.woestenborghs@gmail.com>
2016-03-15 17:47:02 +0100

    Added registrar for the client side bundles

---
+++ Sources/Our.Umbraco.BootEasy.ClientBundling/ClientBundlingRegistrar.cs
@@ -0,0 +1,27 @@
+﻿namespace Our.Umbraco.BootEasy.ClientBundling
+{
+    /// <summary>
+    /// The client bundling registrar.
+    /// </summary>
+    public class ClientBundlingRegistrar : BootRegistrars.ApplicationStartedBootRegistrar
+    {
+        /// <summary>
+        /// Gets the sort order.
+        /// </summary>
+        public override int SortOrder
+        {
+            get
+            {
+                return 99;
+            } 
+        }
+
+        /// <summary>
+        /// Registers the clientside bundles
+        /// </summary>
+        public override void Register()
+        {
+            
+        }
+    }
+}
