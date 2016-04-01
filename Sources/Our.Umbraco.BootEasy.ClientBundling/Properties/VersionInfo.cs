3ee036b8c0a5285ec34ab9cf09b3c7c888763bed
Dave Woestenborghs <dave.woestenborghs@gmail.com>
2016-04-01 09:54:40 +0200

    Added package action to package manifest xml to do config transform

--- Sources/Our.Umbraco.BootEasy.ClientBundling/Properties/VersionInfo.cs
+++ Sources/Our.Umbraco.BootEasy.ClientBundling/Properties/VersionInfo.cs
@@ -14,7 +14,7 @@ using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 [assembly: AssemblyVersion("1.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.26901")]
+[assembly: AssemblyFileVersion("1.0.0.27034")]
 [assembly: AssemblyInformationalVersion("1.0.0")]
 
 
