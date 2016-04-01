959fda8a51383b36b93e4458f37191dbab8196f0
Dave Woestenborghs <dave.woestenborghs@gmail.com>
2016-04-01 09:43:50 +0200

    Include xml transform dll in to umbraco package

--- Sources/Our.Umbraco.BootEasy.ClientBundling/Properties/VersionInfo.cs
+++ Sources/Our.Umbraco.BootEasy.ClientBundling/Properties/VersionInfo.cs
@@ -14,7 +14,7 @@ using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 [assembly: AssemblyVersion("1.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.25592")]
+[assembly: AssemblyFileVersion("1.0.0.26520")]
 [assembly: AssemblyInformationalVersion("1.0.0")]
 
 
