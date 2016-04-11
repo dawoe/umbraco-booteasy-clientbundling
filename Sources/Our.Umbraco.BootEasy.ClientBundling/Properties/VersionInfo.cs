d161e5122f790a15d9a5f0a24085c2d0dce0d95a
Dave Woestenborghs <dave.woestenborghs@gmail.com>
2016-04-11 08:19:35 +0200

    Added documentation

--- Sources/Our.Umbraco.BootEasy.ClientBundling/Properties/VersionInfo.cs
+++ Sources/Our.Umbraco.BootEasy.ClientBundling/Properties/VersionInfo.cs
@@ -14,7 +14,7 @@ using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 [assembly: AssemblyVersion("1.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.24018")]
+[assembly: AssemblyFileVersion("1.0.0.24152")]
 [assembly: AssemblyInformationalVersion("1.0.0")]
 
 
